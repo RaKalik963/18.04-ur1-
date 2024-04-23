20e5884bd35e9a672d710e4a436fb749a013b630
RaKalik963 <andrejmahot646@gmail.com>
2024-04-23 21:35:19 +0300

    dz-ex2

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
+	cout << "\tEvery" << "\n";
+	cout << "\tHunter" << "\n";
+	cout << "\tWants" << "\n";
+	cout << "\tTo know" << "\n";
+	cout << "\tWhere" << "\n";
+	cout << "\tDoes Pheasant" << "\n";
+	cout << "\tSit" << "\n";
 	system("pause");
 	return 0;
 }
