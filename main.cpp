c032ec7c312a1b0703532f638fe1527a3fe12a79
RaKalik963 <andrejmahot646@gmail.com>
2024-04-23 22:09:47 +0300

    lab1-ex1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <windows.h>
 using namespace std;
+HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
 
+/*SetConsoleTextAttribute(handle, FOREGROUND_RED);
+*/
 int main()
 {
+	SetConsoleTextAttribute(handle, 0x06);
+	cout << "\"Hello, World!\"" << endl;
+	SetConsoleTextAttribute(handle, 0x0F);
+	cout << "/Bjarne Stroustrup/" << endl;
 	system("pause");
 	return 0;
 }
