5befb669669cbf42ef2ffe9457b7e5b78c1ece84
RaKalik963 <andrejmahot646@gmail.com>
2024-04-23 22:23:01 +0300

    lab-ex3

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <windows.h>
 using namespace std;
+HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
 
 int main()
 {
+	cout << "To be,";
+	SetConsoleTextAttribute(handle, 0x0D);
+	cout << "or not";
+	SetConsoleTextAttribute(handle, 0x0F);
+	cout << "to be: that is the question:\n";
+	cout << "Whether";
+	SetConsoleTextAttribute(handle, 0x0E);
+	cout << " 'tis nobler in the mind to suffer\n";
+	cout << "The slings and arrows of outrageous fortune,\n";
+	cout << "Or to take arms against a sea of troubles,\n";
+	cout << "And by opposing end them?\n";
+	cout << "William Shakespeare" << endl;
 	system("pause");
 	return 0;
 }
